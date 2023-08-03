7fa05654012f83ce4636897aa3ba59bc6b7671a8
Shivam Pai <72920537+shivampai@users.noreply.github.com>
2023-08-03 22:54:35 +0530

    Add files via upload

---
+++ pragyaan_ui.py
@@ -0,0 +1,55 @@
+import tkinter as tk
+from tkinter import messagebox
+import json
+
+def file_new():
+    messagebox.showinfo("New", "New File")
+
+def soil_sensor_toggle():
+    if 1==1:
+        messagebox.showinfo('Success','Soil Moisture Sensor Deploy Success!')
+    else:
+        messagebox.showerror('Failure','Soil Moisture Sensor Deploy Failure!')
+def file_save():
+    messagebox.showinfo("Save", "Save File")
+
+def moisture_json():
+    data = {
+    "name": "John Doe",
+    "age": 30,
+    "email": "john.doe@example.com",
+    "is_student": True
+    }
+    file_path = "moisture.json"
+    with open(file_path, 'w') as json_file:
+      json.dump(data, json_file, indent=4)
+    messagebox.showinfo('File Save Success','Saved As moisture.json file')
+def headlights_toggle():
+    if 1 == 1:
+        messagebox.showinfo("Success","Headlight Start Success!")
+
+# Create the main application window
+root = tk.Tk()
+root.title("Chandrayaan 3 Rover")
+root.geometry('600x600')
+# Create the Menu Bar
+menu_bar = tk.Menu(root)
+root.config(menu=menu_bar)
+
+# Create the File menu with sub-items
+rover_controls_menu = tk.Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Controls", menu=rover_controls_menu)
+rover_controls_menu.add_command(label="Headlights Toggle", command=headlights_toggle)
+rover_controls_menu.add_command(label="Soil Sample Testing Bar Toggle", command=soil_sensor_toggle)
+rover_controls_menu.add_command(label="Save", command=file_save)
+rover_controls_menu.add_separator()
+rover_controls_menu.add_command(label="Exit", command=root.quit)
+
+# Create the Help menu with sub-items
+
+data_menu = tk.Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Data", menu=data_menu)
+data_menu.add_command(label="Save moisture as .json", command=moisture_json)
+
+# Start the main event loop
+root.mainloop()
