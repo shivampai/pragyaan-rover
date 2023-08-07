8346d7081cef8945a436571fc16dfe1b337914ac
Shivam Pai <72920537+shivampai@users.noreply.github.com>
2023-08-07 23:23:54 +0530

    Update pragyaan_ui.py

--- pragyaan_ui.py
+++ pragyaan_ui.py
@@ -62,9 +62,6 @@ def create_temp_chart():
     canvas.draw()
     canvas.get_tk_widget().pack()
 
-def file_new():
-    messagebox.showinfo("New", "New File")
-
 def start_temp_hum():
     #while True:
     #for i in 10:
@@ -79,7 +76,17 @@ def temp_hum_start():
     data = ser.readline().decode().strip()
     data_list = data.split(",")
     print("temp:",data_list[0],"hum:",data_list[1])
+def deploy_flag():
+    message="f"
+    ser.write(message.encode())  # Encode the message as bytes before sending
+    print(f"Sent message: {message}")
+    data = ser.readline().decode().strip()
+    print("Received data:", data)
 
+    if data=='2001':
+        messagebox.showinfo('Success','Soil Moisture Sensor Deployed Successfully!')
+    elif data=='2002':
+        messagebox.showinfo('Success','Soil Moisture Sensor Retracted Successfully!')
 def soil_sensor_toggle():
     message="m"
     ser.write(message.encode())  # Encode the message as bytes before sending
@@ -90,7 +97,7 @@ def soil_sensor_toggle():
     if data=='2001':
         messagebox.showinfo('Success','Soil Moisture Sensor Deployed Successfully!')
     elif data=='2002':
-        messagebox.showerror('Success','Soil Moisture Sensor Retracted Successfully!')
+        messagebox.showinfo('Success','Soil Moisture Sensor Retracted Successfully!')
 def file_save():
     messagebox.showinfo("Save", "Save File")
 
@@ -103,7 +110,50 @@ def moisture_json():
     with open(file_path, 'w') as json_file:
       json.dump(data, json_file, indent=4)
     messagebox.showinfo('File Save Success','Saved As moisture.json file')
+def hud_data():
+    message="a"
+    ser.write(message.encode())
+    print(f"Sent message: {message}")
     
+    data = ser.readline().decode().strip()
+    if data:
+     data_list = data.split(",")
+     data_list.remove('all')
+     if data_list[5] == "0":
+         data_list[5] = 'Off'
+     else:
+         data_list[5] = 'On'
+     if data_list[6] == "0":
+        data_list[6] = 'Retracted'
+     else:
+        data_list[6] = 'Deployed'
+     if data_list[7] == "0":
+         data_list[7] = 'Retracted'
+     else:
+         data_list[7] = 'Deployed'
+       
+     ter_label.config(text=data_list[0]+"cm")
+     cel_label.config(text=data_list[1]+"℃")
+     soil_moisture_label.config(text=data_list[2]+"%")
+     hum_label.config(text=data_list[3]+"%")
+     com_label.config(text=data_list[4])
+     hl_label.config(text=data_list[5])
+     sm_label.config(text=data_list[6])
+     fa_label.config(text=data_list[7])
+     ofl_label.config(text=data_list[8])
+     ofr_label.config(text=data_list[9])
+    
+#def display_image():    
+ #   image_path = "assets/map_marked.png"  # Replace with the path to your image
+  #  image = Image.open(image_path)
+#
+ #   tk_image = ImageTk.PhotoImage(image)
+  #  label5 = Label(root, image=tk_image)
+   # label5.place(relx=0.5,rely=0.65,anchor=CENTER)
+
+    #label5.image = tk_image
+    #root.mainloop()
+
 def headlights_toggle():
     message="h"
     ser.write(message.encode())  # Encode the message as bytes before sending
@@ -114,7 +164,7 @@ def headlights_toggle():
     if data=='2001':
         messagebox.showinfo('Success','Headlight Turned On Successfully!')
     elif data=='2002':
-        messagebox.showerror('Success','Headlight Turned Off Successfully!')
+        messagebox.showinfo('Success','Headlight Turned Off Successfully!')
 
 # Create the main application window
 
@@ -130,6 +180,7 @@ rover_controls_menu = Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label="Controls", menu=rover_controls_menu)
 rover_controls_menu.add_command(label="Headlights Toggle", command=headlights_toggle)
 rover_controls_menu.add_command(label="Soil Sample Testing Bar Toggle", command=soil_sensor_toggle)
+rover_controls_menu.add_command(label="Deploy Flag", command=deploy_flag)
 rover_controls_menu.add_command(label="Humidity & Temperature", command=start_temp_hum)
 rover_controls_menu.add_command(label="Save", command=file_save)
 rover_controls_menu.add_separator()
@@ -142,6 +193,8 @@ menu_bar.add_cascade(label="Data", menu=data_menu)
 data_menu.add_command(label="Save moisture as .json", command=moisture_json)
 
 
+hud_btn = Button(root,text="Update",command=hud_data)
+hud_btn.place(relx=0.5,rely=0)
 
 label1 = Label(root, text="Terrain (-cm) (ChaTeS)")
 label1.place(relx=0.1,rely=0.05,anchor=CENTER)
@@ -178,13 +231,6 @@ com_label_text = "Healthy"
 com_label = Label(root,font=("Helvetica", 24), text=com_label_text,fg="green")
 com_label.place(relx=0.9,rely=0.09,anchor=CENTER)
     
-label1 = Label(root, text="Headlights")
-label1.place(relx=0.1,rely=0.15,anchor=CENTER)
-  
-hl_label_text = "On"
-hl_label = Label(root,font=("Helvetica", 24), text=hl_label_text)
-hl_label.place(relx=0.1,rely=0.19,anchor=CENTER)
-    
 label1 = Label(root, text="PraSoM Sensor Arm")
 label1.place(relx=0.1,rely=0.15,anchor=CENTER)
   
@@ -213,13 +259,24 @@ ofr_label_text = "False"
 ofr_label = Label(root,font=("Helvetica", 24), text=ofr_label_text)
 ofr_label.place(relx=0.7,rely=0.19,anchor=CENTER)
     
-label1 = Label(root, text="Location Relative")
-label1.place(relx=0.9,rely=0.15,anchor=CENTER)
+#label1 = Label(root, text="Location Relative")
+#label1.place(relx=0.9,rely=0.15,anchor=CENTER)
   
-lr_label_text = "14m"
-lr_label = Label(root,font=("Helvetica", 24), text=lr_label_text)
-lr_label.place(relx=0.9,rely=0.19,anchor=CENTER)
-        
+#lr_label_text = "14m"
+#lr_label = Label(root,font=("Helvetica", 24), text=lr_label_text)
+#lr_label.place(relx=0.9,rely=0.19,anchor=CENTER)
+label1 = Label(root, text="Headlights")
+label1.place(relx=0.9,rely=0.15,anchor=CENTER)
 
+hl_label_text = "On"
+hl_label = Label(root,font=("Helvetica", 24), text=hl_label_text)
+hl_label.place(relx=0.9,rely=0.175)
 
+  
+'''
+for i in range(100):
+   hud_data()
+   timer.sleep(1)
+   '''
+  
 root.mainloop()
